12a97f089532cc1a28fefa138b1cc7d619590837
Subbotin Eugene <subbbbotin983@gmail.com>
2024-08-25 17:35:42 +0300

    Add modals

--- src/js/script.js
+++ src/js/script.js
@@ -37,3 +37,18 @@ slider.goTo('next');
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
 
+//Modal
+
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+      $('#order .modal__descrp').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+  });
+});
